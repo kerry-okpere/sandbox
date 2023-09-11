a88f73c3bbec3723fb1feca9945b4ed15f1b27e8
kerry-okpere <kerryokpere@gmail.com>
2023-09-11 02:38:53 +0100

    feat: rename cards component

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps({
+  maxWidth: {
+    type: String,
+    default: '504px'
+  },
+  hasDropShadow: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div class="card" :class="{ shadow: hasDropShadow }">
+    <slot />
+  </div>
+</template>
+<style lang="scss" scoped>
+.card {
+  background-color: $white;
+  padding: spacing(3);
+  max-width: v-bind(maxWidth);
+  width: 100%;
+  border-radius: spacing(2);
+}
+
+.shadow {
+  box-shadow: 0px 10px 20px 8px rgba(31, 41, 55, 0.08);
+}
+</style>
