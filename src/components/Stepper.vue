170f73a65b38473660ac3e3687b9be191adb09b9
kerry-okpere <kerryokpere@gmail.com>
2023-09-12 03:02:28 +0100

    feat: create stepper component

---
+++ src/components/Stepper.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { PropType } from 'vue';
+import { StepperDirection } from '@/types'
+import Typography from './Typography.vue';
+
+defineProps({
+  content: {
+    type: Array,
+    default: []
+  },
+  activePosition: {
+    type: Number,
+    default: -1
+  },
+  direction: {
+    type: String as PropType<`${StepperDirection}`>,
+    default: StepperDirection.Horizontal
+  },
+  gap: {
+    type: String,
+    default: '0px'
+  },
+  strokeWidth: {
+    type: String,
+    default: '96px'
+  },
+})
+
+</script>
+<template>
+  <ul class="stepper">
+    <li v-for="(item, index) in content">
+      <Typography as="span" weight="700" :class="{ active: index <= activePosition }" class="stepper__item">
+        {{ item }}
+      </Typography>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.stepper {
+  align-items: center;
+  display: flex;
+
+  &__item {
+    background-color: $gray400;
+    width: spacing(3);
+    height: spacing(3);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50vh;
+  }
+
+  &>li {
+    display: flex;
+    align-items: center;
+
+    &:not(:last-child):after {
+      content: "";
+      display: block;
+      height: spacing(0.25);
+      width: v-bind(strokeWidth);
+      background-color: $gray400;
+      margin-right: v-bind(gap);
+      margin-left: v-bind(gap);
+    }
+  }
+}
+
+.active {
+  background-color: $primary;
+}
+</style>
